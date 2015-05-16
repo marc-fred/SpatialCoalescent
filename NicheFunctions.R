06089f524128dc7093276e055994325f29f083bb
Becheler <arnaud.becheler@laposte.net>
2015-05-16 15:55:41 +0200

    function to construct the demographic matrix

--- NicheFunctions.R
+++ NicheFunctions.R
@@ -3,10 +3,31 @@ constant <- function(x,Y)
   # Compute a constant response
   #
   # Args:
-  #   x: numeric providing the values of variable to calculate reaction norm
-  #   Y: value of the constant response
+  #   x : numeric providing the values of variable to calculate reaction norm
+  #   Y : value of the constant response
   #
   # Returns:
   #   The value of the reaction norm
   return(Y)
+}
+
+constructEnvironmentalDemographicMatrix <- function(env, param){
+  # Compute a demographic variable from environmental data, with constant niche function
+  #
+  # Args :
+  #   env : a matrix of the environmental variable values.
+  #   param : the parameter Y of the constant niche function
+  #
+  # Returns :
+  #   A matrix giving the value of the computed demographic variable
+  
+  matrix <- apply(env, c(1,2), constant, Y = param)
+  matrix <- round(matrix)
+  
+  # Prevent impossible transition :
+  if( sum(matrix) <= 0){
+    stop(paste("The parameters values for niche models led to null demographic variable over all cells of the landscape :
+             coalescence is impossible to simulate"))
+  }
+  return(matrix)
 }
