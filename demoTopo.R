c2ce207648b52b500ad657a4086cb2e4e8c47a13
Becheler <arnaud.becheler@laposte.net>
2015-06-20 18:59:23 +0200

    a demonstration file

---
+++ demoTopo.R
@@ -0,0 +1,55 @@
+source("CoalescentFunctions.R")
+source("NicheFunctions.R")
+source("DispersionFunctions.R")
+source("MutationFunctions.R")
+source("PriorFunctions.R")
+source("MarkovProcess.R")
+source("generalFunctions.R")
+source("demographicGrowth.R")
+library(raster)
+library(parallel)
+
+# "Real" Data
+rasterE1 <- raster(x = matrix(data = sample(1:100, 9), ncol = 3),
+                   xmn = 40, xmx = 50, ymn = 0, ymx = 10, crs = "+proj=longlat +datum=WGS84")
+
+rasterE2 <- raster(x = matrix(data = sample(1:100, 9), ncol = 3),
+                   xmn = 40, xmx = 50, ymn = 0, ymx = 10, crs = "+proj=longlat +datum=WGS84")
+
+dataCoord <- xyFromCell(rasterE1, sample(1:ncell(rasterE1), 20, replace = TRUE))
+localizationData <- cellFromXY(rasterE1, dataCoord)
+
+nbLocus <- 10
+steps <- sample(1:10, size = nbLocus ) 
+
+# Model Implementation I
+
+E1 <- as.matrix(rasterE1)
+E2 <- as.matrix(rasterE2)
+land <- E1 ; land[] <- NA
+
+R_m <- demoEnvironmentalVariableConstructor(list(E1, linearTwoParameters, list(X0 = 3, slope = 2)),
+                                            list(E2, gaussianNiche, list(mean = 0, sd = 1)))
+
+K_m <- demoEnvironmentalVariableConstructor(list(E1, constant, list(Y = 20)))
+
+dist_m <- distanceMatrixFromRaster(object = rasterE1)/1000
+M_m <- migrationMatrixConstructor(list(dist_m , gaussianDisp, list(sd = 1)))
+
+demoInit <- createInitialDemographicsLandscape(land)
+
+history <- demographicSimulation(numberOfGenerations = 10,
+                                  demographicMatrix = demoInit, 
+                                  kMatrix = K_m, 
+                                  rMatrix = R_m,
+                                  migMatrix = M_m)
+
+demoHistory_l <- history$demoHistory
+migHistory_l <- history$migHistory
+
+genetValues <- spatialCoalescenceForMultipleLoci(migHistory_l = migHistory_l, 
+                                                 demoHistory_l = demoHistory_l, 
+                                                 localizationData = localizationData, 
+                                                 nbLocus = nbLocus, 
+                                                 theta_rate = runif(n = nbLocus, min=0.1, max = 0.5),
+                                                 steps = steps)
