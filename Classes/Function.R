8e5affdf048b47a7e72e837c3a513bbf4165a5ca
Becheler <arnaud.becheler@laposte.net>
2015-06-21 12:24:35 +0200

    show method OK

--- Classes/Function.R
+++ Classes/Function.R
@@ -1,9 +1,26 @@
 setClass("Function",
-         representation(f = "function"),
-         prototype(f = function(x){}),
+         representation(name = "character",
+                        fun = "function",
+                        param = "list"),
+         prototype(name = "", fun = function(x){}, param = list()),
                    validity = function(object) { ## object : nom reserve !
                      if ( FALSE)
                        return(FALSE)
                      else
                        return(TRUE)
                    })
+
+setMethod("show", "Function",
+          function(object){
+            cat("Function", object@name, "with parameters","\t")
+            pnames <- lapply(X = 1:length(object@param),
+                             FUN = function(x, l){ paste(names(l)[x], "=", l[x], sep ="")},
+                             l = object@param
+            )
+            for(i in 1:length(pnames)){
+              cat(pnames[[i]], "\t" )
+            }
+          })
+
+new("Function", fun = dnorm, param = list(mean = 0, sd = 1))
+new("Function", name = "linear", fun = linearTwoParameters, param = list(X0 = 0, slope = 1))
