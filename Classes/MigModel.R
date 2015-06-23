e2f53e10939c42ab9eedb1a634ada394d10f3d2d
Arnaud Becheler <arnaudbecheler@laposte.net>
2015-06-23 17:20:02 +0200

    formating classfor better legibility

--- Classes/MigModel.R
+++ Classes/MigModel.R
@@ -1,27 +1,30 @@
 setClass("MigModel", contains = "SuperModel")
 
 setMethod(
+  f="ToStream",
+  signature = "MigModel",
+  definition = function(object){
+    cat("Mig model --------------------------------------- \n")
+    callNextMethod(object)
+  }
+)
+
+setMethod(
+  f="getParameters",
+  signature = "MigModel",
+  definition = function(object){
+    p <-  sapply(X = object@models,
+                 FUN = function(x){paste("Mig.", getParameters(x), sep="")})
+    return(as.vector(p))
+  }
+)
+
+setMethod(
   f = "applyModel",
   signature = "MigModel",
   definition =  function(object){
-    callNextMethod()
+    callNextMethod(object)
     migRates <- meanVal/rowSums(meanVal)
     return(migRates)
-  })
-
-setMethod(f="ToStream",
-          signature = "MigModel",
-          definition = function(object){
-            cat("Migration model --------------------------------------- \n")
-            callNextMethod()
-          }
-)
-
-setMethod(f="getParameters",
-          signature = "MigModel",
-          definition = function(object){
-            p <-  sapply(X = object@models,
-                         FUN = function(x){paste("Mig.", getParameters(x), sep="")})
-            return(as.vector(p))
-          }
+  }
 )
